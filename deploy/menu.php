0851b9376e5fd1db41c2829516e9a9e2e69f7891
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-03-12 03:09:47 +0000

    - Moved process viewer to separate package

--- deploy/menu.php
+++ deploy/menu.php
@@ -1,9 +1,5 @@
 <?php
 
-$menu['/app/base/processes']['title'] = menu('base', 'base_processes');
-$menu['/app/base/processes']['category'] = menu('menu', 'menu_category_system');
-$menu['/app/base/processes']['subcategory'] = menu('menu', 'menu_subcategory_resources');
-
 $menu['/app/base/dashboard']['title'] = menu('menu', 'base_overview');
-$menu['/app/base/dashboard']['category'] = menu('menu', 'menu_category_reports');
+$menu['/app/base/dashboard']['category'] = menu('menu', 'menu_category_system');
 $menu['/app/base/dashboard']['subcategory'] = menu('menu', 'menu_subcategory_dashboard');
