3cb11f2fed519c0f7ba8da3929421172a6dbbec2
Aaron Bylund <abylund@clearcenter.com>
2011-04-20 18:19:55 +0000

    - Converted

---
+++ libraries/OS.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Operating system class.
+ *
+ * @category   Apps
+ * @package    Base
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2006-2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/base/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\base;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('base');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_library('base/Engine');
+clearos_load_library('base/File');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Operating system class.
+ *
+ * @category   Apps
+ * @package    Base
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2006-2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/base/
+ */
+
+class Os extends Engine
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    // V A R I A B L E S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    private $os = NULL;
+    private $version = NULL;
+    private $previous_os = NULL;
+    private $previous_version = NULL;
+
+    const FILE_CORE_RELEASE = '/etc/clearos-release';
+    const FILE_RELEASE = '/etc/clearos-release';
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // M E T H O D S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Os constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+    }
+
+    /**
+     * Returns the name of the operating system/distribution.
+     *
+     *
+     * @return string OS name
+     * @throws Engine_Exception
+     */
+
+    public function get_name()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        if (is_null($this->os))
+            $this->_load_config();
+
+        return $this->os;
+    }
+
+    /**
+     * Returns the version of the operating system/distribution.
+     *
+     *
+     * @return string OS version
+     * @throws Engine_Exception
+     */
+
+    public function get_version()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        if (is_null($this->version))
+            $this->_load_config();
+
+        return $this->version;
+    }
+
+    /**
+     * Returns the technical version of the operating system/distribution.
+     *
+     *
+     * @return string technical version
+     * @throws Engine_Exception
+     */
+
+    public function get_core_version()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        try {
+            $file = new File(Os::FILE_CORE_RELEASE);
+            $contents = $file->get_contents();
+        } catch (File_Not_Found_Exception $e) {
+            return $this->get_version();
+        } catch (Exception $e) {
+            throw new Engine_Exception(clearos_exception_message($e));
+        }
+
+        $osinfo = explode(" release ", $contents);
+
+        if (count($osinfo) != 2)
+            throw new Engine_Exception(OS_LANG_ERRMSG_NAME_UNKNOWN);
+
+        return $osinfo[1];
+    }
+
+    /**
+     * Populates version and name fields.
+     *
+     * @access private
+     * @throws Engine_Exception
+     */
+
+    protected function _load_config()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        $file = new File(self::FILE_RELEASE);
+        $contents = $file->get_contents();
+
+        $osinfo = explode(" release ", $contents);
+
+        if (count($osinfo) != 2)
+            throw new Engine_Exception(OS_LANG_ERRMSG_NAME_UNKNOWN);
+
+        $this->os = $osinfo[0];
+        $this->version = $osinfo[1];
+    }
+}
