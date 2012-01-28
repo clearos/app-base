07d448eb534e7b154b0afb7d533ca0d533d84693
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-01-28 16:43:40 +0000

    - Updated wizard intro blurb

--- views/wizard.php
+++ views/wizard.php
@@ -40,33 +40,51 @@ $this->lang->load('base');
 ///////////////////////////////////////////////////////////////////////////////
 // FIXME: translate
 
-$right = "
-<h3>Help and User Guide</h3>
-<p>During the install wizard, you will find help and best practices provided as you go through
-the configuration steps:</p>
-
-<p align='left'><img src='" . clearos_app_htdocs('base') . "/inline_help.png' alt=''></p>
-
-<p>In addition, you can also follow the links to the User Guide and 
-Support found near the top of the page:
-<p>
-<p align='left'><img src='" . clearos_app_htdocs('base') . "/help_and_support.png' alt=''></p>
-";
 
 $left = "
-<h3>Thank You</h3>
-<p>You are now ready to install apps and integrated cloud services through the ClearCenter Marketplace.
-You will find both free a paid apps... blah blah blah.</p>
+<h3>" . $this->session->userdata['os_name'] . " " . $this->session->userdata['os_base_version'] . "</h3>
+<p>The install wizard is going to take you through the basic steps to get your " . $this->session->userdata['os_name'] . " system up and running.  Once you have the basics all configured, you will be ready to move on to 
+Marketplace to select apps and services</p>
+";
 
+$help_text = "
 <h3>Getting Started</h3>
-<p>If this is the first
-time using ClearOS, the number of apps and services in the Marketplace can be overwhelming.  The
-Markeplace wizard guides you through the process of selecting the right features.  If you don't 
-like wizards, you can skip this step and jump right into the <a style='background: transparent; border: none; float: none; padding: 0; margin: 0; color: #e1852e;' href='/app/base/wizard/finish'>Marketplace</a>.</p>
-" .
-"<p align='center'>" .  anchor_custom($first_step, 'Start the Wizard') . "</p>";
+<p>During the install wizard, you will find help and best practices provided as you go through
+the configuration steps.</p>
+
+<p><i>Example Help:</i><Br>
+<img src='" . clearos_app_htdocs('base') . "/inline_help.png' alt=''></p>
+
+<p>In addition, you can also follow the links to the User Guide and 
+Support found near the top of the page.  Let's get started!</p>
+";
+
+$start_wizard = "<p align='center'>" .  anchor_custom($first_step, 'Start the Wizard') . "</p>";
+
+// FIXME: this blurb should change on ClearBOX hardware
+$right = "
+<h3>Looking for Hardware? Take a Look at ClearBOX...</h3>
+<p>Here comes the shameless plug! ClearBOX is a high performance server from ClearCenter specifically designed to run the ClearOS platform. We designed this LInux IT server with ClearOS in mind and frankly, we're proud of our work! ClearBOX delivers leading technology and performance to leverage the strengths of ClearOS.</p>
+
+<p><a style='background: transparent; border: none; float: none; padding: 0; margin: 0; color: #e1852e;' href='http://www.clearcenter.com/clearbox'>Read More...</a></p>
+
+
+<p align='center'><img src='" . clearos_app_htdocs('base') . "/clearbox-product.jpg' alt='ClearBOX'></p>
+";
 
 echo infobox_highlight(
     'Welcome to the Install Wizard!',
-    "<table cellpadding='7'><tr><td valign='top'>$left</td><td valign='top' width='265'>$right</td></tr></table>"
+    "<table cellpadding='7' border='0'>
+        <tr>
+            <td valign='top' colspan='2'>$left</td>
+            <td valign='top' width='360' rowspan='3'>$right</td>
+        </tr>
+        <tr>
+            <td valign='top'>$help_text</td>
+            <td valign='top'>$help_graphic</td>
+        </tr>
+        <tr>
+            <td valign='top' colspan='2'>$start_wizard</td>
+        </tr>
+    </table>"
 );
