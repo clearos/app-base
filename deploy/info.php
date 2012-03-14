c1482450b54686cf250eb0a481385e9cbae24396
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-14 21:58:42 +0000

    - Added flag to syncaction

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.0.7';
+$app['version'] = '1.0.8';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
