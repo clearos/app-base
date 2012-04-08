4394b9e9a4aa8963d73d977784fc883ded8874a6
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-08 01:15:50 +0000

    - Prepped for bug fix release

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.0.13';
+$app['version'] = '1.0.15';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
