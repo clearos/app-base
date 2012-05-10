f37de65fa17d8d4771f750e245216ff41fef93e9
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-05-10 22:00:57 +0000

    - Prep for Zarafa build and devel environment

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.1.2';
+$app['version'] = '1.1.4';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
