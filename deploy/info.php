d76a01fe2d0b9f261997a0acc0aec24a003eed1e
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-10-26 19:36:08 +0000

    - Prepped for internal build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.2.8';
+$app['version'] = '1.4.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
