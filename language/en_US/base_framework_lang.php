3d2153b4254ddbbbf40d57a8001397ae4499f4c5
Ben Chambers <bchambers@clearfoundation.com>
2012-11-01 19:46:40 +0000

    - Added bytes to base_bytes mapping

--- language/en_US/base_framework_lang.php
+++ language/en_US/base_framework_lang.php
@@ -1,5 +1,6 @@
 <?php
 $lang['required'] = $lang['base_this_field_required'];
+$lang['bytes'] = $lang['base_bytes'];
 $lang['terabyte_abbr'] = $lang['base_terabytes'];
 $lang['gigabyte_abbr'] = $lang['base_gigabytes'];
 $lang['megabyte_abbr'] = $lang['base_megabytes'];
