8f2b5aab9b3f7008d7239dfd3a13bf6cab69cdbe
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-30 21:35:07 +0000

    - Prepped version for pre-beta 1 build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.4.6';
+$app['version'] = '1.4.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 1.4.6
+Version: 1.4.7
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
