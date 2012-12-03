4687ac0caf179fb3b80cc770a2030b1f0b62074d
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-12-03 19:01:08 +0000

    - Added generic tuning library

---
+++ libraries/Tuning.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * System tuning class.
+ *
+ * @category   Apps
+ * @package    Base
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/base/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\base;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('base');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+// Classes
+//--------
+
+use \clearos\apps\base\Engine as Engine;
+use \clearos\apps\base\File as File;
+
+clearos_load_library('base/Engine');
+clearos_load_library('base/File');
+
+// Exceptions
+//-----------
+
+use \clearos\apps\base\Engine_Exception as Engine_Exception;
+
+clearos_load_library('base/Engine_Exception');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * System tuning class.
+ *
+ * @category   Apps
+ * @package    Base
+ * @subpackage Libraries
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/base/
+ */
+
+class Tuning extends Engine
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    // C O N S T A N T S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    const LEVEL_HOME_NETWORK  = 'home_network';
+    const LEVEL_LIGHT  = 'light';
+    const LEVEL_SMALL  = 'small';
+    const LEVEL_MEDIUM  = 'medium';
+    const LEVEL_LARGE  = 'large';
+    const LEVEL_VERY_LARGE  = 'very_large';
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // V A R I A B L E S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    protected $levels = array();
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // M E T H O D S
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Tuning constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        $this->levels = array(
+            self::LEVEL_HOME_NETWORK => lang('base_home_network'),
+            self::LEVEL_LIGHT => lang('base_light_duty'),
+            self::LEVEL_SMALL => lang('base_small'),
+            self::LEVEL_MEDIUM => lang('base_medium'),
+            self::LEVEL_LARGE => lang('base_large'),
+            self::LEVEL_VERY_LARGE => lang('base_extra_large'),
+        );
+
+    }
+
+    /**
+     * Returns the tuning levels.
+     *
+     * @return array tuning levels
+     * @throws Engine_Exception
+     */
+
+    public function get_levels()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        return $this->levels;
+    }
+}
