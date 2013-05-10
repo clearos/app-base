76cdc504283a9946eb678e4c5d92617b2c5c3829
Ben Chambers <bchambers@clearfoundation.com>
2013-05-10 14:42:27 +0000

    - Bumped version for Marketplace dependency

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.4.37';
+$app['version'] = '1.4.38';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
