3776190438afe86c7c50f45e353c69dcdf11d37d
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-05-17 15:37:07 +0000

    - Bumped version due to backport

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.4.38';
+$app['version'] = '1.4.39';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
