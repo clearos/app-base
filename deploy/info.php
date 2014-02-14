cd2dc37976605ffb7e576df8b0fbdbaefc3b00c0
pcbaldwin <pbaldwin@clearfoundation.com>
2014-02-13 22:00:06 -0500

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.5.24';
+$app['version'] = '1.5.25';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 1.5.24
+Version: 1.5.25
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
