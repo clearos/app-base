87cadb8303d167fd140c01d441b15a0de04de97c
pcbaldwin <pbaldwin@clearfoundation.com>
2014-06-13 18:20:56 -0400

    Re-enable beta repos

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.3';
+$app['version'] = '1.6.4';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 1.6.3
+Version: 1.6.4
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
