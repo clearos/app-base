ada51bbe1823b0c08e65a006215936add166138c
pcbaldwin <pbaldwin@clearfoundation.com>
2014-10-31 17:03:29 -0400

    With the slight change in edition workflow, change logic

--- libraries/Install_Wizard.php
+++ libraries/Install_Wizard.php
@@ -216,7 +216,7 @@ class Install_Wizard extends Engine
             $os_name = $os->get_name();
             $os_version = $os->get_version();
 
-            if (preg_match('/ClearOS Community/', $os_name) && !preg_match('/(beta|alpha|rc)/i', $os_version)) {
+            if (!preg_match('/ClearOS Professional/', $os_name))
                 $steps[] = array(
                     'nav' => '/app/edition',
                     'title' => lang('edition_select_edition'),
