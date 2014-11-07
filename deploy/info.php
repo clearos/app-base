184152ac32d472529f11eb5387a40de81d9840b0
pcbaldwin <pbaldwin@clearfoundation.com>
2014-11-07 16:09:21 -0500

    Added packages to EPEL exclude list [tracker #1977]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.5';
+$app['version'] = '1.6.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 1.6.5
+Version: 1.6.6
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
