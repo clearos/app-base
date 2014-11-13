6deb63f38447260fdd9219d69992c09711838dca
pcbaldwin <pbaldwin@clearfoundation.com>
2014-11-13 00:12:24 -0500

    Prepping for 6.6.0 Beta 2

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.6';
+$app['version'] = '1.6.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
