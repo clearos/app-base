f2dd417d067903bdd9fbf20adf5bf6573d6d13ca
pcbaldwin <pbaldwin@clearfoundation.com>
2015-01-25 21:50:31 -0500

    Resolved premature push of app-base 1.6.9 to updates-testing

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.9';
+$app['version'] = '1.6.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 1.6.9
+Version: 1.6.10
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
