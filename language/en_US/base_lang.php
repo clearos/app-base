5d1b61985e8bd9d622ba558197ae7f32da1c5cde
Benjamin Chambers <bchambers@clearfoundation.com>
2015-05-19 14:02:14 -0400

    Adde tag

--- language/en_US/base_lang.php
+++ language/en_US/base_lang.php
@@ -522,6 +522,7 @@ $lang['base_version'] = 'Version';
 $lang['base_very_high'] = 'Very High';
 $lang['base_very_low'] = 'Very Low';
 $lang['base_view'] = 'View';
+$lang['base_view_all_events'] = 'View all critical events';
 $lang['base_view_cart'] = 'View Cart';
 $lang['base_view_details'] = 'View Details';
 $lang['base_virtual_machine'] = 'Virtual Machine';
