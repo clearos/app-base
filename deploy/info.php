9cef41e5354201b6faec2d6c717a4f2d1d06692e
pcbaldwin <pbaldwin@clearfoundation.com>
2015-06-11 01:51:35 -0400

    Fixed version problems with wc-yum (backport) [tracker #2043]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.11';
+$app['version'] = '1.6.12';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
