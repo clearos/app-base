1019e5bfc51ef39cd1923e22dc374488bfbc441d
Benjamin Chambers <bchambers@clearfoundation.com>
2015-06-24 11:18:53 -0400

    Perform disk space check based on percentage of free space remaining Perform disk space check only on write operations...ignore read

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.6.13';
+$app['version'] = '1.6.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
