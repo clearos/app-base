d7a9498a435fe4a013ba09d52d04fc7a8de267d5
pcbaldwin <pbaldwin@clearfoundation.com>
2015-08-13 14:15:14 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.7.0';
+$app['version'] = '1.7.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
