34a5f188648e000d1e3ed6f7ca1c8ee3d0be3440
Benjamin Chambers <bchambers@clearfoundation.com>
2015-09-09 13:20:40 -0400

    Fixed language tag

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.1.17';
+$app['version'] = '2.1.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
