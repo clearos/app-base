f2a36950f8e96ac3a54fbbf42b05b66f6237ea4d
pcbaldwin <pbaldwin@clearfoundation.com>
2015-11-13 00:05:50 -0500

    Added owncloud to EPEL exclude

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.7.6';
+$app['version'] = '1.7.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
