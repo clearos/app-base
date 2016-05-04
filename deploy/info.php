586d8f6428e3560c75abd13ca235f75f7d8bde25
pcbaldwin <peter@egloo.ca>
2016-05-03 21:24:56 -0400

    Added shim/grub2 yum exclude upgrade [tracker #8251]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.2.5';
+$app['version'] = '2.2.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 2.2.5
+Version: 2.2.6
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
