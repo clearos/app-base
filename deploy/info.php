6c5f5dcff0de4631bdb021080b51d03dfa328ec1
pcbaldwin <peter@egloo.ca>
2016-06-09 10:48:08 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.2.8';
+$app['version'] = '2.2.9';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
