cb131d4bb84ff1e54ef8995cf91a171f5563547d
pcbaldwin <pbaldwin@clearfoundation.com>
2016-06-09 16:23:09 -0400

    All good with testing, enable beta repos

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '1.8.0';
+$app['version'] = '1.8.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
