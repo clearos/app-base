a9d7e9426575b28b9f190a1163cf41746c21c268
pcbaldwin <peter@egloo.ca>
2016-08-23 13:41:15 -0400

    Bumped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.2.12';
+$app['version'] = '2.2.13';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
