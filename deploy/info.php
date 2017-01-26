b21c62203faed5ca0972f140e1da1b4c15764aa7
Benjamin Chambers <bchambers@clearfoundation.com>
2017-01-26 09:26:37 -0500

    Updated links and changed logo [Tracker 12601]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.7';
+$app['version'] = '2.3.8';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
