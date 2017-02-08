58e9a53ffe276afc6a6a509c17620c0017dddf22
pcbaldwin <peter@egloo.ca>
2017-02-07 21:18:08 -0500

    Waiting on critical path, roll back to Beta 1

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.9';
+$app['version'] = '2.3.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
