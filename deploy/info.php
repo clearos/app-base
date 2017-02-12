fb6158559f0327af822038a04d7f7595813ecac1
pcbaldwin <peter@egloo.ca>
2017-02-12 12:40:21 -0500

    Re-rebuild with beta enabled

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.10';
+$app['version'] = '2.3.11';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
