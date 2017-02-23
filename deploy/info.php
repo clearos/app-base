bdd8c33c05be7fc41037e23f7c66aa5cbabc3f9b
pcbaldwin <peter@egloo.ca>
2017-02-23 15:59:21 -0500

    Improved language support in sessions

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.15';
+$app['version'] = '2.3.20';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
