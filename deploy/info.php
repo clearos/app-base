e9260e83bd7ae6d03f79d8dd969fae288f9e42e4
pcbaldwin <peter@egloo.ca>
2017-03-02 11:12:19 -0500

    Updated translations

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.21';
+$app['version'] = '2.3.22';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
