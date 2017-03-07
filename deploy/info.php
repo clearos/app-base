ae76aa5eb3043269d665c270bb73aa5171759a30
pcbaldwin <peter@egloo.ca>
2017-03-07 14:07:42 -0500

    Bumped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.24';
+$app['version'] = '2.3.27';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 2.3.24
+Version: 2.3.27
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
