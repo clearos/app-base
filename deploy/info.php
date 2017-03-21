79f8487c86caba7197da59ab07260cfe3f155400
pcbaldwin <peter@egloo.ca>
2017-03-21 11:39:44 -0400

    Fixed sudoers duplicates [tracker #13941]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.30';
+$app['version'] = '2.3.32';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
