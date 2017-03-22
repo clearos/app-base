c2b200626ab49b51aedbe1d10babea90078a163a
pcbaldwin <peter@egloo.ca>
2017-03-22 10:52:15 -0400

    Rebuild

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.32';
+$app['version'] = '2.3.33';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
