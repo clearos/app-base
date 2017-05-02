a045df55bbadf8f537634af722f5a055280833a2
pcbaldwin <peter@egloo.ca>
2017-05-02 11:10:59 -0400

    Added storage check [tracker #14611]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.3.36';
+$app['version'] = '2.3.37';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-base.spec
+++ packaging/app-base.spec
@@ -1,7 +1,7 @@
 
 Name: app-base
 Epoch: 1
-Version: 2.3.36
+Version: 2.3.37
 Release: 1%{dist}
 Summary: General Settings
 License: GPLv3
