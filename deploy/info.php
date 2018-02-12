e93518a12b458b3552e6457347020b4bd37eca5d
pcbaldwin <peter@egloo.ca>
2018-02-12 09:18:44 -0500

    Bumped for buildsys

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'base';
-$app['version'] = '2.4.17';
+$app['version'] = '2.4.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
